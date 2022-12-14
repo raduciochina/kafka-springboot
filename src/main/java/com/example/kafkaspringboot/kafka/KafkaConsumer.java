cc49eaf26014f098c80f59058df1b8e2d8c9873a
rciochin <ciochina7@gmail.com>
2022-12-14 15:48:05 +0200

    2nd commit

--- src/main/java/com/example/kafkaspringboot/kafka/KafkaConsumer.java
+++ src/main/java/com/example/kafkaspringboot/kafka/KafkaConsumer.java
@@ -1,12 +1,18 @@
 package com.example.kafkaspringboot.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics="exampleTopic", groupId = "myGroup")
     public void consumeMessage(String message){
-        System.out.println("Consumed message: " + message);
+        LOGGER.info(String.format("$$ -> Consumed Message -> %s", message));
     }
 
 }
