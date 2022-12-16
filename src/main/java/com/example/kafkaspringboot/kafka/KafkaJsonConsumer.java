236adac9fc2cd7f1513dd51d1d93d190aec37ee1
rciochin <ciochina7@gmail.com>
2022-12-16 11:45:24 +0200

    4th commit ->> final

--- src/main/java/com/example/kafkaspringboot/kafka/KafkaJsonConsumer.java
+++ src/main/java/com/example/kafkaspringboot/kafka/KafkaJsonConsumer.java
@@ -1,7 +1,18 @@
 package com.example.kafkaspringboot.kafka;
 
+import com.example.kafkaspringboot.payload.User;
+import org.slf4j.Logger;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaJsonConsumer {
+
+    private static Logger LOGGER = org.slf4j.LoggerFactory.getLogger(KafkaJsonConsumer.class);
+
+    @KafkaListener(topics="exampleTopicJson", groupId = "myGroup")
+    public void consume(User user){
+        LOGGER.info(String.format("$$ -> JSON Consumed Message -> %s", user.toString()));
+    }
+
 }
